1a418148ac8716e0f6bf4a9213d8ca788fa71936
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2020-09-10 02:30:26 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -134,6 +134,10 @@ REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
+# DefaulDeDefaultfaultt .alive logo
+ALIVE_LOGO = os.environ.get("ALIVE_LOGO") or None
+
+
 # Chrome stuff
 CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/usr/bin/chromedriver")
 GOOGLE_CHROME_BIN = os.environ.get(
