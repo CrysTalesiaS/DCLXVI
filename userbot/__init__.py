477e419662109d486a423c0c8975d802ec5869c3
CrysTalesiaS <53529472+CrysTalesiaS@users.noreply.github.com>
2020-09-10 13:40:15 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -231,7 +231,10 @@ WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY", None)
 # Deezloader
 DEEZER_ARL_TOKEN = os.environ.get("DEEZER_ARL_TOKEN", None)
 
-
+# Image for alive
+IMG = os.environ.get(
+    "IMG",
+    "https://t.me/AnimeDiskusIndo/19034")
 
 # Set default timezone
 TZ = os.environ.get("TZ", "Asia/Jakarta")
